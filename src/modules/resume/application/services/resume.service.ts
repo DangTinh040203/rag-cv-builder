7aa56a4800e17135454f69ec670591dbae1289ba
DangTinh42 <caodangtinh04022003@gmail.com>
2026-01-22 22:00:10 +0700

    update: rename file

---
+++ src/modules/resume/application/services/resume.service.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import {
+  type IResumeRepository,
+  RESUME_REPOSITORY_TOKEN,
+} from '@/modules/resume/application/interfaces';
+import { Resume } from '@/modules/resume/domain';
+import {
+  CreateResumeDto,
+  UpdateResumeDto,
+} from '@/modules/resume/presentation/DTOs';
+
+@Injectable()
+export class ResumeService {
+  constructor(
+    @Inject(RESUME_REPOSITORY_TOKEN)
+    private readonly resumeRepository: IResumeRepository,
+  ) {}
+
+  create(userId: string, payload: CreateResumeDto): Promise<Resume> {
+    return this.resumeRepository.create(userId, payload);
+  }
+
+  update(id: string, payload: UpdateResumeDto): Promise<Resume> {
+    return this.resumeRepository.update(id, payload);
+  }
+}
--- src/modules/resume/presentation/controllers/resume.controller.ts
+++ src/modules/resume/presentation/controllers/resume.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+
+import { ResumeService } from '@/modules/resume/application/services';
+import { UpdateResumeDto } from '@/modules/resume/presentation/DTOs';
 
 @Controller('resume')
-export class ResumeController {}
+export class ResumeController {
+  constructor(private readonly resumeService: ResumeService) {}
+
+  @Post('/:id')
+  async update(@Param('id') id: string, @Body() payload: UpdateResumeDto) {
+    return this.resumeService.update(id, payload);
+  }
+}
