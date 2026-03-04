68a46cd81c9975d399adc7a6c7f2790d0d9ee588
DangTinh42 <caodangtinh04022003@gmail.com>
2026-03-04 21:26:20 +0700

    refactor: Extract CacheService into a dedicated cache.server.ts file.

--- src/libs/cache/cache.module.ts
+++ src/libs/cache/cache.module.ts
@@ -1,41 +1,13 @@
 import KeyvRedis from '@keyv/redis';
-import {
-  CACHE_MANAGER,
-  CacheModule as NestCacheModule,
-} from '@nestjs/cache-manager';
-import { Global, Inject, Injectable, Module } from '@nestjs/common';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
+import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Keyv } from 'keyv';
 
+import { CacheService } from '@/libs/cache/cache.server';
 import { Env } from '@/libs/configs/env.config';
 
 export const DEFAULT_CACHE_TTL = 5 * 60 * 1000;
-
-interface CacheStore {
-  get<T>(key: string): Promise<T | undefined>;
-  set<T>(key: string, value: T, ttl?: number): Promise<void>;
-  del(key: string): Promise<void>;
-}
-
-@Injectable()
-export class CacheService {
-  constructor(
-    @Inject(CACHE_MANAGER)
-    private readonly cache: CacheStore,
-  ) {}
-
-  async get<T>(key: string): Promise<T | undefined> {
-    return this.cache.get<T>(key);
-  }
-
-  async set<T>(key: string, value: T, ttl?: number): Promise<void> {
-    await this.cache.set(key, value, ttl ?? DEFAULT_CACHE_TTL);
-  }
-
-  async del(key: string): Promise<void> {
-    await this.cache.del(key);
-  }
-}
 
 @Global()
 @Module({
--- src/libs/cache/index.ts
+++ src/libs/cache/index.ts
@@ -1,6 +1,3 @@
 export { CacheKeys, CachePrefix } from '@/libs/cache/cache.keys';
-export {
-  CacheModule,
-  CacheService,
-  DEFAULT_CACHE_TTL,
-} from '@/libs/cache/cache.module';
+export { CacheModule, DEFAULT_CACHE_TTL } from '@/libs/cache/cache.module';
+export { CacheService } from '@/libs/cache/cache.server';
